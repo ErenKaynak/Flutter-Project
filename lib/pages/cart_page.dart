b1e65c8026d32db4f6430f095fe8fc6f74da459e
ErenKaynak <erenkaynak@ogrenci.beykoz.edu.tr>
2025-03-12 11:45:22 +0300

    cart page text fix
    sadece yazıyı düzelttim :D

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,7 +12,7 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-        Center(child: Text('Profile Page',))
+        Center(child: Text('Cart Page',))
       );
   }
 }
