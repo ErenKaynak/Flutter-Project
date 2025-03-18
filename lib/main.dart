966256a0a8c132cb8a8f4220a814fb19327a488c
Deniz Zeybek <denizzeybek1905@gmail.com>
2025-03-18 13:01:44 +0300

    Debug Flaair
    Its closed now

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:engineering_project/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -22,6 +23,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: true, home: RootScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: RootScreen());
   }
 }
